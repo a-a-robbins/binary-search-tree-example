0e23b280fb1e0a975079111f61c1395d7ed6f5a9
Ashley Robbins <robbinsa1@central.edu>
2022-10-11 15:57:28 -0500

    create Node class

--- BST.java
+++ BST.java
@@ -1,5 +1,16 @@
 public class BST {
 
+    class Node {
+        int key; //contains value of the node
+        Node left, right, parent; //how we point to the Nodes
+
+        //create a Node
+        Node(int value) {
+            key = value;
+            left = right = null; 
+        }
+    }
+
     public static void main(String[] args) {
         
     }
